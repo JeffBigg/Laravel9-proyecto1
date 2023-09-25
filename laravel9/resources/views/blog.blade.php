79fd28c76318c6d26a10adb3f4a244b3eb4b486d
Jeferson Soria <soriaarandajeferson@gmail.com>
2023-09-25 14:26:16 -0500

    agregando plantillas

--- laravel9/resources/views/blog.blade.php
+++ laravel9/resources/views/blog.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-
-<body>
-    @include('partials.navigation')
+@extends('layouts.app')
+@section('title', 'blog')
+@section('meta-description', 'blog')
+@section('content')
     <h1>Blog</h1>
-</body>
-
-</html>
+@endsection
--- laravel9/resources/views/contact.blade.php
+++ laravel9/resources/views/contact.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contacto</title>
-</head>
-
-<body>
-    @include('partials.navigation')
+@extends('layouts.app')
+@section('title', 'contact')
+@section('meta-description', 'contact')
+@section('content')
     <h1>Contacto</h1>
-</body>
-
-</html>
+@endsection
